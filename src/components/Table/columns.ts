4d0fccd15360b275f5e8510fb1f52b3d7b2f6afa
SDen4 <asw325@yandex.ru>
2021-05-25 22:14:03 +0500

    format cell example

--- src/components/Table/columns.ts
+++ src/components/Table/columns.ts
@@ -2,5 +2,11 @@ export const GROUP_COLUMNS: Array<any> = [
   { Header: 'Bank', accessor: 'title' },
   { Header: 'VALUE 1', accessor: 'value1' },
   { Header: 'VALUE 2', accessor: 'value2' },
-  { Header: 'VALUE 3', accessor: 'value3' },
+  {
+    Header: 'VALUE 3',
+    accessor: 'value3',
+    Cell: (cell: any) => {
+      return cell.value + '%';
+    },
+  },
 ];
