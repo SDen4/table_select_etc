525d22ed971c0bbc379261ecf0d01f259388b08c
SDen4 <asw325@yandex.ru>
2021-05-29 20:42:50 +0500

    the array of the last table row

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,14 +1,32 @@
 import React, { useMemo } from 'react';
 import { useTable } from 'react-table';
 
-import { GROUP_COLUMNS } from './columns';
-
 import { TableProps } from './types';
 
 import './Table.module.css';
 
 const Table: React.FC<TableProps> = ({ tableData }) => {
-  const columns = useMemo(() => GROUP_COLUMNS, []);
+  const GROUP_COLUMNS: Array<any> = useMemo(
+    () => [
+      {
+        Header: 'Bank',
+        accessor: 'title',
+      },
+      { Header: 'VALUE 1', accessor: 'value1' },
+      { Header: 'VALUE 2', accessor: 'value2' },
+      {
+        Header: 'VALUE 3',
+        accessor: 'value3',
+        Cell: (cell: any) => {
+          console.log(cell.rows[rows.length - 1].cells); // Массив последней строки
+          return cell.value + '%';
+        },
+      },
+    ],
+    [],
+  );
+
+  const columns = useMemo(() => GROUP_COLUMNS, [GROUP_COLUMNS]);
   const data = useMemo(() => tableData, [tableData]);
 
   const tableInstance = useTable({
@@ -26,7 +44,9 @@ const Table: React.FC<TableProps> = ({ tableData }) => {
           {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                <th {...column.getHeaderProps([])}>
+                  {column.render('Header')}
+                </th>
               ))}
             </tr>
           ))}
